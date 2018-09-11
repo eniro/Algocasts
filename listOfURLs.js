5daa5ffcf8846006e08ad060ced035491e91c123
Iro Enezli <enezliro@gmail.com>
2018-09-11 21:58:11 +0300

    added reverseInt

---
+++ listOfURLs.js
@@ -0,0 +1,29 @@
+// Problem: Display list in HTML. Target id of my "mylists".
+
+// var arr = [
+//     { 
+//         name: "LinkedIn",
+//         link: "http://linkedin.com"
+//     },
+//     {
+//         name: "Blog",
+//         link: "http://blog.com"
+//     },
+//     {
+//         name: "Twitter",
+//         link: "http:twitter.com"
+//     }
+// ];
+
+
+
+var myTarget = document.querySelector("#mylists");
+
+Array.forEach(function(item) {
+    var link = document.createElement('a');
+    link.href = item.link;
+    link.innerText = item.name;
+
+    myTarget.appendChild(link);
+})
+
